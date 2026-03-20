14b797f9a8b60a30b7fb16d26c261332015dfa7b
Vijay Balaji <cs5231103@iitd.ac.in>
2026-03-20 15:21:15 +0530

    made a working thing but very bad efficiency

--- partb/bpe_tokenizer.py
+++ partb/bpe_tokenizer.py
@@ -1,4 +1,52 @@
 from heapq import heapify, heappop, heappush
+import json
+import os
+
+def find_freq(str_corpus, word):
+    freq = 0
+    for i in range(len(str_corpus) - len(word) + 1):
+        if word == str_corpus[i:i+len(word)]:
+            freq+=1
+    return freq
+
+def merge_data(t1, t2):
+    return t1+t2
+
+class Node:
+    def __init__(self, data = -1, next = None, prev = None):
+        self.data = data
+        self.next = next
+        self.prev = prev
+
+class DLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def append(self, data):
+        if self.head == None:
+            self.head = Node(data)
+            self.tail = self.head
+        else:
+            self.tail.next = Node(data, prev = self.tail)
+            self.tail = self.tail.next
+    
+    def merge(self, node, merge_data):  #merge node and its next node to form a single node
+        if node.next != None:
+            data1 = node.data
+            data2 = node.next.data
+            new_data = merge_data(data1, data2)
+            new_node = Node(new_data, prev=node.prev, next=node.next.next)
+            if self.head == node:
+                self.head = new_node
+            else:
+                node.prev.next = new_node
+            if self.tail == node.next:
+                self.tail = new_node
+            else:
+                node.next.next.prev = new_node
+            return new_node
+            
 
 class BPETokenizer:
     def __init__(self, vocab_size, special_tokens=None):
@@ -7,8 +55,8 @@ class BPETokenizer:
         self.unk_id = None #set it in train
         self.merge_rules = [] #set it in train
         self.vocab = [] #same as before
-        self.devocab = [] #same as before
-        self.special_tokens = []
+        self.devocab = {} #same as before
+        self.special_tokens = special_tokens
 
     def train(self, corpus):
         # raise NotImplementedError("Training method not implemented yet.")
@@ -24,12 +72,12 @@ class BPETokenizer:
             sentence = list(sentence)
             bigram_corpus.extend([(sentence[i-1],sentence[i]) for i in range(1, len(sentence))])
             sent_vocab = set(sentence)
-            vocab.union(sent_vocab)
+            vocab.update(sent_vocab)
         merged_pairs = {}
         for bigram in bigram_corpus:
             merged_pairs[bigram] = merged_pairs.get(bigram, 0) + 1
-        merged_pairs = [(merged_pairs[bigram], bigram) for bigram in merged_pairs.keys()]
-        merged_pairs_heap = heapify(merged_pairs)
+        merged_pairs_heap = [(-merged_pairs[bigram], bigram) for bigram in merged_pairs.keys()]
+        heapify(merged_pairs_heap)
         for i in range(self.vocab_size - len(vocab)):
             try:
                 max_pair = heappop(merged_pairs_heap)
@@ -37,34 +85,95 @@ class BPETokenizer:
                 break
             bigram_sep = max_pair[1]
             bigram = "".join(bigram_sep)
+            vocab.add(bigram)
             merge_rules.append(bigram_sep)
             for token in vocab:
                 word1 = token + bigram
                 word2 = bigram + token
-                freq1, freq2 = find_freq(str_corpus, word1, word2) #to be implemented
+                # freq1, freq2 = find_freq(str_corpus, word1, word2) #to be implemented
+                freq1 = find_freq(str_corpus, word1)
+                freq2 = find_freq(str_corpus, word2)
                 if freq1 > 0:
-                    merged_pairs_heap.heappush((freq1,(token, bigram)))
+                    # merged_pairs_heap.heappush((-freq1,(token, bigram)))
+                    heappush(merged_pairs_heap, (-freq1,(token, bigram)))
                 if freq2 > 0:
-                    merged_pairs_heap.heappush((freq2, (bigram, token)))
+                    # merged_pairs_heap.heappush((-freq2, (bigram, token)))
+                    heappush(merged_pairs_heap, (-freq2,(bigram, token)))
+        self.vocab = list(vocab)
+        self.merge_rules = merge_rules
+        #add unknown and special things
+        self.vocab.append("<UNK>")
+        self.unk_id = len(self.vocab) - 1
+        # special_tokens = set(self.special_tokens)
+        # self.vocab.extend(special_tokens)
+        if self.special_tokens != None:
+            if isinstance(self.special_tokens, list):
+                self.vocab.extend(self.special_tokens)
+            else:
+                self.vocab.append(self.special_tokens)
+        
+        #set devocab
+        self.devocab = {}
+        for i in range(len(self.vocab)):
+            self.devocab[self.vocab[i]] = i
                     
         
             
         
     
     def encode(self, text):
-        raise NotImplementedError("Encoding method not implemented yet.")
+        # raise NotImplementedError("Encoding method not implemented yet.")
+        #text is a single string - line of text 
+        #need to split it up into character level first 
+        #need to iterate thru the merge rules and combine whatever can be combined
+        #nextiterate thru the text adn convert them to ids based on devocab
+        #return the list of ids
+        chars = list(text)
+        DLlist = DLinkedList()
+        for char in chars:
+            DLlist.append(char)
+        for rule in self.merge_rules:
+            node = DLlist.head
+            while node!=DLlist.tail and node != None:
+                if rule == (node.data, node.next.data):
+                    node = DLlist.merge(node, merge_data)
+                node = node.next
+        token_ids = []
+        node = DLlist.head
+        while node!=None:
+            token_ids.append(self.devocab.get(node.data, self.unk_id))
+            node = node.next
+        return token_ids
+        pass
 
     def decode(self, token_ids):
-        raise NotImplementedError("Decoding method not implemented yet.")
+        # raise NotImplementedError("Decoding method not implemented yet.")
+        #you will get a list of ints in token ids
+        #conver it into a listof str
+        #convert that back into a single text by "".join(list)
+        list_of_tokens = list(map(lambda x: self.vocab[x], token_ids))
+        # print(token_ids, self.vocab, self.devocab)
+        return "".join(list_of_tokens)
 
     def save(self, filepath):
-        raise NotImplementedError("Save method not implemented yet.")
+        # raise NotImplementedError("Save method not implemented yet.")
+        filename = os.path.join(filepath, "tokenizer.json")
+        with open(filename, "w") as fh:
+            json.dump(self.__dict__, fh)
+        pass
 
     def load(self, filepath):
-        raise NotImplementedError("Load method not implemented yet.")
+        # raise NotImplementedError("Load method not implemented yet.")
+        filename = os.path.join(filepath, "tokenizer.json")
+        with open(filename, "r") as fh:
+            tokeniser = json.load(fh)
+            self.__dict__.update(tokeniser)
+        pass
     
     def get_vocab_size(self):
-        raise NotImplementedError("Get vocab size method not implemented yet.")
+        # raise NotImplementedError("Get vocab size method not implemented yet.")
+        return self.vocab_size
     
     def get_unk_id(self):
-        raise NotImplementedError("Get unk id method not implemented yet.")
+        # raise NotImplementedError("Get unk id method not implemented yet.")
+        return self.unk_id
